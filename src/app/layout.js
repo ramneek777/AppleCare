e4b5cc62dd69e980d6ee7b34c96289b4844a187e
Ramneek Marwaha <ramneek1001@hpinc.com>
2023-12-25 16:29:56 +0530

    meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="xC3h4fz6ZvBHSXJc23BDTgiwdQVLG0SAnA8NqKjodHg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
